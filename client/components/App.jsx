0d53093a8f876aceb69b6b27e1844e17f293e37a
Taylor Wagstaff <taylorwagstaff@gmail.com>
2022-09-13 19:02:54 +1200

    blockmarks

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,25 +1,66 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
-import { fetchFruits, selectFruits } from '../slices/fruits'
+import React from 'react'
+import ASCII from 'react-rainbow-ascii'
+import FireFont from 'figlet/importable-fonts/Fire Font-k'
+import Acrobatic from 'figlet/importable-fonts/Acrobatic'
+import Alligator from 'figlet/importable-fonts/Alligator'
+import Bubble from 'figlet/importable-fonts/Bubble'
 
 function App() {
-  const fruits = useSelector(selectFruits)
-  const dispatch = useDispatch()
-
-  useEffect(async () => {
-    await dispatch(fetchFruits())
-  }, [])
-
   return (
     <>
-      <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {fruits.map((fruit) => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
+      <div className="header">
+        <img src="images/logo.jpg" className="logo" alt="logo" />
+        <h1>/taylors_folder</h1>
+      </div>
+
+      <div className="ascii">
+        <a
+          href="https://github.com/localjo/react-rainbow-ascii/blob/master/examples.md"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ASCII
+            text="https://github.com/localjo/react-rainbow-ascii/blob/master/examples.md"
+            rainbow={false}
+          />
+        </a>
+        <a href="https://www.google.com/" target="_blank" rel="noreferrer">
+          <ASCII
+            text="https://www.google.com/"
+            rainbow={false}
+            font={FireFont}
+          />
+        </a>
+
+        <a
+          href="https://www.npmjs.com/package/ascii-art"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ASCII
+            text="https://www.npmjs.com/package/ascii-art"
+            rainbow={false}
+            font={Acrobatic}
+          />
+        </a>
+
+        <a
+          href="https://jakedowsmith.studio/wikipedia/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ASCII
+            text="https://jakedowsmith.studio/wikipedia/"
+            rainbow={false}
+            font={Alligator}
+          />
+        </a>
+
+        <img src="images/meme.png" className="image" alt="meme" />
+
+        <a href="https://decrypt.co/?" target="_blank" rel="noreferrer">
+          <ASCII text="https://decrypt.co/?" rainbow={false} font={Bubble} />
+        </a>
       </div>
     </>
   )
